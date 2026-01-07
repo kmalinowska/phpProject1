9785d15a7a02eb5a150ab3991312afb0768f8a1a
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-01-07 18:33:20 +0100

    add examples expressions

--- expressions.php
+++ expressions.php
@@ -1,1 +1,17 @@
-\
+<?php
+
+echo "Welcome to PHP\n";
+
+$name = "Alice";
+echo "Hello, " . $name . "!\n";
+
+$pizzas = 3;
+$slicesPerPizza = 8;
+$totalSlices = $pizzas * $slicesPerPizza;
+
+echo "Total pizza slices: " . $totalSlices . "\n";
+
+$isHungry = true;
+echo "Hungry? ";
+echo $isHungry ? "Yes" : "No";
+echo "\n";
