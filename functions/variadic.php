764959969ef0f36daaa3098474fcf2877b260428
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-01-09 19:07:40 +0100

    use strict types in requier and variadic arguments

--- functions/variadic.php
+++ functions/variadic.php
@@ -1,6 +1,8 @@
 <?php
 
-function sum(...$numbers) {
+declare(strict_types=1);
+
+function sum(int ...$numbers):int {
     $sum = 0;
     var_dump($numbers);
     foreach($numbers as $number) {
@@ -13,7 +15,8 @@ var_dump(sum());
 var_dump(sum(5));
 var_dump(sum(5, 10, 15, 20, 25));
 
-function introduceTeam($teamName, ...$members) {
+//this funcion don't return anything, we should use :void
+function introduceTeam(string $teamName, string ...$members):void {
     echo "Team: $teamName\n";
     var_dump($members);
     echo "Members: " . implode(", ", $members) . "\n";
