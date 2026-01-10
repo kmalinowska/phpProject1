03bb9232492225b8b8d1b52d9e66a5366f349291
Klaudia Malinowska <kmalinowska@protonmail.com>
2026-01-10 12:59:44 +0100

    add named.php file with named arguments example

---
+++ functions/named.php
@@ -0,0 +1,20 @@
+<?php
+
+function greet(string $name, string $greeting = "Hello", bool $shout = false): string {
+    $message = "$greeting, $name!";
+    return $shout ? strtoupper($message): $message;
+}
+
+echo greet("Alice") . "\n";
+echo greet("Alice", "Hi") . "\n";
+echo greet("Alice", "Hey", true) . "\n";
+
+echo greet(name: "David", shout:false);
+
+/* use named arguments:
+output:
+Hello, Alice!
+Hi, Alice!
+HEY, ALICE!
+Hello, David!
+*/
